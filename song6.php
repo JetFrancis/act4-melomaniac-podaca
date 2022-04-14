697fa01bca7fc2c03dcf4015b9023d853efc4b36
POdacaJetFrancis <jetfrancispodaca@gmail.com>
2022-04-15 06:06:58 +0800

    Song's lyrics added & text modification

---
+++ song6.php
@@ -0,0 +1,157 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-danger fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Tune'f Music </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light " href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song01.php">Song1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song02.php">Song2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song03.php">Song3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song04.php">Song4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song05.php">Song5</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song06.php">Song6</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+        <!-- The Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-3 mb-0 text-center">
+
+                <!--logo-->
+                <img src="images/icon01.PNG" width="50" height="50">
+                <h3 class="lead">SONG 6</h3>
+
+                <!--Tittle-->
+                <?php echo "<br/>" ?>
+                <h1 class='font-italic'>"That Should Be Me"</h1>  
+                
+            </div>
+        </header>
+        <style>
+            body {
+                background: #654ea3;
+                background: linear-gradient(to bottom, #ffefba, #ffffff);
+            }
+        </style>
+
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <img src="images/icon02.PNG" width="100" height="100">
+                    
+                        <?php
+
+                        echo "<h2 class='text-primary'>That Should Be Me</h2>";  
+                        echo "<p class='lead'>Justin Bieber</p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Intro]</p>";
+                        echo "<p>[Bm] [G] [D] [A] </p>";
+                        echo "<br/>";
+                        echo "<p class='small'>[Verse]</p>";
+                        echo "<p>[Bm]Everybody's [G]laughing in my [A]mind [D] [A] </p>";
+                        echo "<p>[Bm]Rumors spreading [G]about this other [A]guy [D] [A] </p>";
+                        echo "<p>Do you [Bm]do what you did what you [G]did with me</p>";
+                        echo "<p>[Does he [A]love you the way I [D]can [A] </p>";
+                        echo "<p>Did you [Bm]forget all the plans that [G]you made with me</p>";
+                        echo "<p>Cause [A]baby I [D]didn't [A] </p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Chorus]</p>";
+                        echo "<p>That should be [D]me holding your hand</p>";
+                        echo "<p>[That should be [A]me making you laugh</p>";
+                        echo "<p>That should be [Bm]me this is so sad</p>";
+                        echo "<p>That should be [G]me</p>";
+                        echo "<p>That should be [A]me</p>";
+                        echo "<br/>";
+
+                        echo "<p>That should be [D]me feeling your kiss</p>";
+                        echo "<p>That should be [A]me buying you gifts</p>";
+                        echo "<p>This is so [Bm]wrong</p>";
+                        echo "<p>[I can't go [Bm]on</p>";
+                        echo "<p>'Till you [G]believe</p>";
+                        echo "<p>That [A]that should be [D]me</p>";
+                        echo "<p>That should be [A]me</p>";
+                        echo "<br/>";
+
+                         echo "<p class='small'>[Verse]</p>";
+                        echo "<p>You s[Bm]aid you needed a </p>";
+                        echo "<p>little t[G]ime from my mista[A]kes [D] [A] </p>";
+                        echo "<p>It's f[Bm]unny how you used that</p>";
+                        echo "<p>At[G]ime to have me repla[A]ced [D] [A] </p>";
+                        echo "<p>Did you [Bm]think that I</p>";
+                        echo "<p>wouldn't see you [G]out at the movies </p>";
+                        echo "<br/>";
+
+                        echo "<p>[A]What you doin' to me</p>";
+                        echo "<p>You're [D]taking him where [A]we used to go</p>";
+                        echo "<p>[Bm]Now if you're trying to [G]break my heart</p>";
+                        echo "<p>[B]It's [A]working cause you [D]know [A] </p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Chorus]</p>";
+                        echo "<p>That should be [D]me holding your hand</p>";
+                        echo "<p>[That should be [A]me making you laugh</p>";
+                        echo "<p>That should be [Bm]me this is so sad</p>";
+                        echo "<p>That should be [G]me</p>";
+                        echo "<p>That should be [A]me</p>";
+                        echo "<br/>";
+
+                        echo "<p>That should be [D]me feeling your kiss</p>";
+                        echo "<p>That should be [A]me buying you gifts</p>";
+                        echo "<p>This is so [Bm]wrong</p>";
+                        echo "<p>[I can't go [Bm]on</p>";
+                        echo "<p>'Till you [G]believe</p>";
+                        echo "<p>That [A]that should be [D]me</p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Bridge]</p>";
+                        echo "<p>[F]I need to know should I [G]fight for love Or disarm</p>";
+                        echo "<p>[F]It's getting harder to [G]shield</p>";
+                        echo "<p>This pain is my [D]heart [A] </p>";
+                        echo "<p>[F]Ooh Ooh</p>";
+                        echo "<br/>";
+
+                        ?>
+
+                        <br>
+                        <p class='text-dark fw-bolder'>PLAY</p>
+                        <audio controls>
+                    <source src="audios/sound1.mp3" type="audio/mpeg">
+                </audio> 
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-primary">
+            <div class="container px-4"><p class="m-0 text-center text-dark">Copyright &copy; 2022. Jet Francis Q. Podaca</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
