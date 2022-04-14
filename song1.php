f254b0c8821d056cce25c2316fcdd2f39265db5e
POdacaJetFrancis <jetfrancispodaca@gmail.com>
2022-04-15 05:37:54 +0800

    Newly song added in Song 1 page

---
+++ song1.php
@@ -0,0 +1,86 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-danger fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Tune'f Music </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light " href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song01.php">Song1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song02.php">Song2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song03.php">Song3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song04.php">Song4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song05.php">Song5</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song06.php">Song6</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <img src="images/icon01.PNG" width="50" height="50">
+                <h1 class="fw-bolder">Welcome to Tune'f Music</h1>
+                <h5 class="lead pt-4 mb-0">Be on a sort of time of traveling, sentimental, nostalgic wave of vibration to provide you a well-needed escape to happier times yet to come, here in tune of mixed songs of Tune'f Music.</h5>
+                
+            </div>
+        </header>
+        <style>
+        body {
+                background: #654ea3;
+                background: linear-gradient(to bottom, #ffefba, #ffffff);
+            }
+        </style>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <img src="images/icon02.PNG" width="100" height="100">
+
+                        <h2 class='fw-bolder'>FEATURED SONGS</h2>";<br>
+                        <br/>
+                        <div class="py-8 px-9">
+                        <ul class="list-inline-item mb-0" type='none'>
+                        <li><h7 class='fw-bolder'> Take Me Home, Country Road</h7><i> - John Denver</i></li><br>
+                        <li><h7 class='fw-bolder'> At My Worst</h7><i> - Pinky Sweats</i></li><br>
+                        <li><h7 class='fw-bolder'> Uptown Girl</h7><i> - Billy Joel</i></li><br>
+                        <li><h7 class='fw-bolder'> More Than Words</h7><i> - Westlife</i></li><br>
+                        <li><h7 class='fw-bolder'> I Lay My Love On You</h7><i> - Westlife</i></li><br>
+                        <li><h7 class='fw-bolder'> That Should Be Me</h7><i> - Justin Bieber</i></li><br>
+                        </ul>
+                        </div>
+
+                        
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-primary">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Jet Francis Q. Podaca</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
